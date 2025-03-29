b7c714f8f497458639325eefa844f63a3dadd1fd
Iura Shchekotov <yuriyshchekotov@gmail.com>
2025-03-29 08:11:26 +0200

    chore: migrate for telegraf/filters

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,7 @@
 const { Telegraf } = require('telegraf');
 const startCommand = require('./handlers/start');
 const helpCommand = require('./handlers/help');
+const { message } = require('telegraf/filters');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const { saveMessage } = require('./db/messages');
@@ -9,8 +10,19 @@ const { saveMessage } = require('./db/messages');
 bot.start(startCommand);
 bot.help(helpCommand);
 
-// Все сообщения
-bot.on('message', (ctx) => {
+bot.on(message('text'), (ctx, next) => {
+    const text = ctx.message.text;
+    const knownCommands = ['start', 'help']; // можно подгрузить из commands.js
+    const command = text.split(' ')[0].substring(1); // без '/'
+
+    if (text.startsWith('/') && !knownCommands.includes(command)) {
+        return ctx.reply('Команда не распознана 🤷‍♂️');
+    }
+
+    return next();
+});
+
+bot.on(message(), (ctx) => {
     const from = ctx.from.username || ctx.from.first_name;
     const text = ctx.message.text || '[не текстовое сообщение]';
     const chatId = ctx.chat.id;
